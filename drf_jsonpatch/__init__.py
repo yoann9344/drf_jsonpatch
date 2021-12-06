cd53b7b4a9bfd3e0c4ce70f1c60880e0e38bc2a8
Yoann <yoann9344@gmail.com>
2021-12-07 00:48:59 +0100

    Excecute patch in __init__

--- drf_jsonpatch/__init__.py
+++ drf_jsonpatch/__init__.py
@@ -1,1 +1,6 @@
 from drf_jsonpatch.apply_json_patch import apply_json_patch  # noqa: F401
+
+
+# execute patch
+import drf_jsonpatch.patchs.parser  # noqa: F401
+import drf_jsonpatch.patchs.serialiazers  # noqa: F401
